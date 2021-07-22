545cebbcd733399957bdc365d1f19cca99eb3b6d
junwufan <shelllet.com@gmail.com>
2021-07-22 08:45:47 +0800

    Add project files.

---
+++ TwoWayMode/MainWindow.xaml.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::TwoWayMode::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+	Windows::Foundation::Size MainWindow::MySize()
+	{
+		throw hresult_not_implemented();
+	}
+	void MainWindow::MySize(Windows::Foundation::Size const& value)
+	{
+		throw hresult_not_implemented();
+	}
+	Windows::Foundation::Rect MainWindow::MyRect()
+	{
+		throw hresult_not_implemented();
+	}
+	void MainWindow::MyRect(Windows::Foundation::Rect const& value)
+	{
+		throw hresult_not_implemented();
+	}
+
+    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
---
+++ TwoWayMode/MainWindow.xaml.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#pragma push_macro("GetCurrentTime")
+#undef GetCurrentTime
+
+#include "MainWindow.g.h"
+
+#pragma pop_macro("GetCurrentTime")
+
+namespace winrt::TwoWayMode::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+		Windows::Foundation::Size MySize();
+		void MySize(Windows::Foundation::Size const& value);
+		Windows::Foundation::Rect MyRect();
+		void MyRect(Windows::Foundation::Rect const& value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::TwoWayMode::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
